bdf0dd577217d2b59166a3405382690312a066fd
Teele Kaldaru <Teele.Kaldaru@uptime.eu>
2018-07-03 16:20:15 +0300

    Clean-up

---
+++ Uptime.Auction.Web/Controllers/AuctionController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Uptime.Auction.Web.Controllers
+{
+    [Route("[controller]")]
+    [Route("")]
+    public class AuctionController : Controller
+    {
+
+        [Route("[action]")]
+        [Route("")]
+        public IActionResult Index()
+        {
+            // TODO: nimekiri oksjonitest
+            // id, endtime, algpanus, bootstrap
+            var model = new List<Models.Auction>
+            {
+                new Models.Auction
+                {
+                    Item = "eheh",
+                    Start = DateTime.Now,
+                },
+
+                new Models.Auction
+                {
+                    Item = "ihih",
+                    Start = DateTime.Now,
+                }
+            };
+            return View(model);
+        }
+
+
+        [HttpGet("[action]")]
+        public IActionResult Detail()
+        {
+            // TODO: ühe oksjoni andmed
+            return View();
+        }
+
+        [HttpPost("[action]")]
+        public IActionResult Bid()
+        {
+            // TODO: panustamine
+            return View();
+        }
+
+        [HttpPost("[action]")]
+        public IActionResult Create()
+        {
+            // TODO: oksjoni loomine
+            return View();
+        }
+    }
+}
---
+++ Uptime.Auction.Web/Models/Auction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Uptime.Auction.Web.Models
+{
+    public class Auction
+    {
+        public string Item { get; set; }
+        public DateTime Start { get; set; }
+
+    }
+}

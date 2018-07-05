2d3d294da68d03c9a2d5c5bb1c65f449a4ddde50
Teele Kaldaru <Teele.Kaldaru@uptime.eu>
2018-07-05 15:57:05 +0300

    first tests

---
+++ Uptime.Auction.Core.Test/AuctionServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uptime.Auction.Core.Test
+{
+    [TestClass]
+    public class AuctionServiceTests
+    {
+        private AuctionService auctionService;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            var auctionRepositoryMock = new Mock<IAuctionRepository>();
+            auctionService = new AuctionService(auctionRepositoryMock.Object);
+        }
+
+        [TestMethod]
+        public void TestCreate()
+        {
+            // arrange
+            // act
+            // assert
+            auctionService.Create("suva", 32, DateTime.Now);
+        }
+
+
+    }
+}

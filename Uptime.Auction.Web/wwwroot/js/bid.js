f9bb8326b74a37dbfa019c21a36b84cc12127258
Teele Kaldaru <Teele.Kaldaru@uptime.eu>
2018-07-10 12:03:01 +0300

    form validation added

--- Uptime.Auction.Web/wwwroot/js/bid.js
+++ Uptime.Auction.Web/wwwroot/js/bid.js
@@ -5,6 +5,7 @@
     function bidButtonClicked() {
         var bidPrice = document.getElementById("bid-input");
         var auctionId = bidPrice.getAttribute("data-auction-id");
+
         if (bidPrice.value) {
             $.ajax({
                 type: "POST",
@@ -19,5 +20,31 @@
         }
     }
 
-    bidButton.addEventListener("click", bidButtonClicked, false);
+    if (bidButton)
+        bidButton.addEventListener("click", bidButtonClicked, false);
+
+
+    var form = document.getElementById("create");
+    var date = moment().format("YYYY-MM-DD HH:mm:ss");
+
+    function validateForm(e) {
+        var name = form.elements["Item"].value;
+        var startingPrice = document.forms["create"]["StartingPrice"].value;
+        var endTime = document.forms["create"]["End"].value;
+
+        if (name == "") {
+            alert("Items name must be filled out");
+            e.preventDefault();
+        }
+        else if (startingPrice <= 0 || isNaN(startingPrice)) {
+            alert("Invalid starting price");
+            e.preventDefault();
+        }
+        else if (endTime <= date) {
+            alert("End time must be greater than start time");
+            e.preventDefault();
+        }
+    }
+
+    form.addEventListener("submit", validateForm, false);
 });
